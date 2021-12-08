44c3747466c5362b6aebb192fb44c477253b9a59
Bart Smykla <bartek@smykla.com>
2021-12-08 15:41:51 +0100

    ghactions
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- ghactions.ps1
+++ ghactions.ps1
@@ -18,7 +18,7 @@ Invoke-WebRequest `
   -OutFile actions-runner-win-x64.zip
 
 $wantHash = 'f79dbb6dfae9d42d0befb8cff30a145dd32c9b1df6ff280c9935c46884b001f3'.ToUpper()
-$hash = $(Get-FileHash -Path actions-runner-win-x64.zip -Algorithm SHA256).Hash.ToUpper())
+$hash = $(Get-FileHash -Path actions-runner-win-x64.zip -Algorithm SHA256).Hash.ToUpper()
 
 if(($hash -ne $wantHash) {
   throw 'Computed checksum did not match'
